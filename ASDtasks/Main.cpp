23c1f78601ff43aa95054e00c5337cc659ad9f83
Hellamb <vlad.kuksiuk@gmail.com>
2020-11-26 15:40:36 +0200

    add project

---
+++ ASDtasks/Main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include <vector>
+#include <chrono>
+#include <conio.h>
+
+#include "QuickSorter.h"
+#include "MergeSorter.h"
+
+using namespace std;
+
+void printArray(vector<long> arr);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+    QuickSorter qs;
+    MergeSorter ms;
+
+    vector<long> arr1 = {};
+
+    int numberOfIterations;
+    cout << "Введіть кількість елементів в масиві: ";
+    cin >> numberOfIterations;
+
+    for (int i = 0; i < numberOfIterations; i++)
+    {
+        arr1.push_back(rand() % 100 + 1);
+    }
+    cout << "Оберіть тип сортування" << endl << "1 - Швидке сортування" << endl << "2 - Сортування злиттям" << endl;
+    int sortType;
+    cin >> sortType;
+    vector<long> newArray;
+
+    cout << "Відсортувати відсортований масив?" << endl << "1 - так" << endl << "не 1 - ні" << endl;
+    int isSorted;
+    cin >> isSorted;
+    if (isSorted == 1)
+    {
+        arr1 = qs.sort(arr1);
+    }
+
+    auto begin = chrono::steady_clock::now();
+    if (sortType == 1)
+    {
+        //sort.quickSorting(arr1);
+        newArray = qs.sort(arr1);
+    }
+    else if (sortType == 2)
+    {
+        newArray = ms.sort(arr1);
+    }
+    else
+    {
+        cout << "Error";
+        return 0;
+    }
+    auto end = chrono::steady_clock::now();
+    auto algTime = chrono::duration_cast<chrono::milliseconds>(end - begin);
+    cout << endl;
+    printArray(newArray);
+
+    cout << endl << "Час виконання: " << algTime.count() << " мс";
+
+
+}
+
+
+void printArray(vector<long> array)
+{
+    int arraySize = array.size();
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << array[i] << ", ";
+    }
+}
---
+++ ASDtasks/QuickSorter.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+class QuickSorter
+{
+public:
+    vector<long> sort(vector<long>& arr)
+    {
+        int arrSize = arr.size();
+
+        if (arrSize <= 1)
+        {
+            return arr;
+        }
+        long middleNum = arr[arrSize / 2];
+        vector<long> minArr;
+        vector<long> midArr;
+        vector<long> maxArr;
+
+        for (long num : arr)
+        {
+            if (num < middleNum)
+            {
+                minArr.push_back(num);
+            }
+            else if (num == middleNum)
+            {
+                midArr.push_back(num);
+            }
+            else
+            {
+                maxArr.push_back(num);
+            }
+        }
+        minArr = sort(minArr);
+        maxArr = sort(maxArr);
+
+        minArr.insert(minArr.end(), midArr.begin(), midArr.end());
+        minArr.insert(minArr.end(), maxArr.begin(), maxArr.end());
+        return minArr;
+    }
+};
