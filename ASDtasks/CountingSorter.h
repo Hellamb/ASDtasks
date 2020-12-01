5bf8b1f6a3d6249f4e19e04e53619646c6478141
Philideli <74490209+Philideli@users.noreply.github.com>
2020-12-01 14:57:28 +0300

    Conting sort на .cpp файлах

--- ASDtasks/CountingSorter.h
+++ ASDtasks/CountingSorter.h
@@ -6,22 +6,6 @@ using namespace std;
 class CountingSorter
 {
 public:
-	vector<long> sort(vector<long>& arr) {
-		int k = 100;
-		int n = arr.size();
-		vector<long> c(k+1,0);
-		for (int i = 0; i < n; i++) {
-			c[arr[i]] = c[arr[i]] + 1;
-		}
-
-		int b = 0;
-		for (int i = 0; i < c.size(); i++) {
-			for (int j = 0; j < c[i]; j++) {
-				arr[b] = i;
-				b = b + 1;
-			}
-		}
-		return arr;
-	}
+	vector<long> sort(vector<long>& arr);
 };
 
--- ASDtasks/ShellSorter.h
+++ ASDtasks/ShellSorter.h
@@ -7,22 +7,6 @@ class ShellSorter
 {
 public:
 
-    vector<long> sort(vector<long>& arr)
-    {
-        long size = arr.size();
-        for (int step = size / 2; step > 0; step /= 2)
-        {
-            for (int i = step; i < size; i++)
-            {
-                for (int j = i - step; j >= 0 && arr[j] > arr[j + step]; j -= step)
-                {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + step];
-                    arr[j + step] = tmp;
-                }
-            }
-        }
-        return arr;
-    }
+    vector<long> sort(vector<long>& arr);
 };
 
